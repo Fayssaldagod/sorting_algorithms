51984a506ad1656a3d2515764d3204f3df16d98e
Fayssaldagod <fayssaltube@gmail.com>
2024-01-26 01:30:53 +0100

    test

---
+++ swap_ints.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swaps the two  given integers
+ * @a: The first integer
+ * @b: The second integer
+ */
+
+void swap_ints(int *a,int *b)
+{
+	int tmp;
+	tmp = *a; 
+	*a = *b;
+	*b = tmp;
+}
